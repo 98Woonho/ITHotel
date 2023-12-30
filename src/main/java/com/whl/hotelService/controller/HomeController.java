e117c5d60cd44c663087d06c8f379d58722c2501
98Woonho <9woonho8@gmail.com>
2023-12-30 11:16:12 +0900

    first commit

---
+++ src/main/java/com/whl/hotelService/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.whl.hotelService.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping(value = "home")
+public class HomeController {
+    @GetMapping(value = "homepage")
+    public void getHomepage() {
+        log.info("getHomepage()");
+    }
+}
---
+++ src/main/java/com/whl/hotelService/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.whl.hotelService.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping(value = "user")
+public class UserController {
+    @GetMapping(value = "login")
+    public void getLogin() {
+        log.info("getLogin()");
+    }
+}
