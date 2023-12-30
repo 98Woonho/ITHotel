7bba51ca189e901d460153e47430098b59ebd5a6
98Woonho <9woonho8@gmail.com>
2023-12-30 11:22:45 +0900

    test

--- src/main/java/com/whl/hotelService/controller/UserController.java
+++ src/main/java/com/whl/hotelService/controller/UserController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
     @GetMapping(value = "login")
     public void getLogin() {
-        log.info("getLogin()");
+        log.info("getLogin");
     }
 }
