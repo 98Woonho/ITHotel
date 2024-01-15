df557377cffdf91f9a9eb0185dd7da84a5652407
hbsh2330 <hbsh2330@naver.com>
2024-01-15 18:29:13 +0900

    board

---
+++ src/main/java/com/whl/hotelService/boardDomain/dto/BoardDto.java
@@ -0,0 +1,64 @@
+package com.whl.hotelService.boardDomain.dto;
+
+import com.whl.hotelService.boardDomain.entity.BoardEntity;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+//DTO(Data Transfer Object) 파라미터들을 전송하기 위한 Dto
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor // 기본생성자
+@AllArgsConstructor // 모든필드를 매개변수로 하는 생성자
+public class BoardDto {
+    private Long id;
+    @NotBlank(message = "작성자를 입력하세요")
+    private String boardWriter; //작성자
+    @NotBlank(message = "비밀번호를 입력하세요")
+    private String boardPassword; //비밀번호
+    @NotBlank(message = "제목을 입력하세요")
+    private String boardTitle; //제목
+    @NotBlank(message = "내용을 입력하세요")
+    private String boardContents; //내용
+    private int boardHits; //조회수
+    private LocalDateTime boardCreatedTime; // 작성시간
+    private LocalDateTime boardUpdatedTime; // 수정시간
+
+//    public BoardDto(Long id, String boardWriter, String boardTitle, String boardContents, int boardHits, LocalDateTime boardCreatedTime) {
+//        this.id = id;
+//        this.boardWriter = boardWriter;
+//        this.boardTitle = boardTitle;
+//        this.boardContents = boardContents;
+//        this.boardHits = boardHits;
+//        this.boardCreatedTime = boardCreatedTime;
+//    }
+
+    public BoardDto(Long id, String boardWriter, String boardTitle, int boardHits, LocalDateTime boardCreatedTime) {
+        this.id = id;
+        this.boardWriter = boardWriter;
+        this.boardTitle = boardTitle;
+        this.boardHits = boardHits;
+        this.boardCreatedTime = boardCreatedTime;
+    }
+
+
+
+    public static BoardDto EntityToDto(BoardEntity boardEntity) { //빌더 패턴 사용
+        BoardDto boardDto = BoardDto.builder()
+                .id(boardEntity.getId())
+                .boardTitle(boardEntity.getBoardTitle())
+                .boardContents(boardEntity.getBoardContents())
+                .boardHits(boardEntity.getBoardHits())
+                .boardCreatedTime(boardEntity.getCreatedTime())
+                .boardPassword(boardEntity.getBoardPassword())
+                .boardWriter(boardEntity.getBoardWriter())
+                .boardUpdatedTime(boardEntity.getUpdatedTime())
+                .build();
+        return boardDto;
+    }
+
+}
+
---
+++ src/main/java/com/whl/hotelService/controller/CommentController.java
@@ -0,0 +1,35 @@
+package com.whl.hotelService.controller;
+
+import com.whl.hotelService.boardDomain.dto.CommentDto;
+import com.whl.hotelService.boardDomain.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/comment")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+
+    @PostMapping("/save")
+    public ResponseEntity save(CommentDto commentDto) throws Exception{
+        System.out.println("commentDto : " + commentDto);
+        Long saveResult = commentService.save(commentDto);
+        if (saveResult != null){
+            // 작성 성공 로직 댓글 목록을 가져와서 리턴
+            List<CommentDto> commentDtoList = commentService.findAll(commentDto.getBoardId());
+            return new ResponseEntity<>(commentDtoList, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("해당 게시글이 존재하지 않습니다.", HttpStatus.NOT_FOUND);
+        }
+    }
+}
