ee9fe3b70c5d8790bd439c2a6acdf2f4c8ab686d
98Woonho <9woonho8@gmail.com>
2024-01-17 21:02:24 +0900

    Entity Update

---
+++ src/main/java/com/whl/hotelService/domain/common/entity/Reservation.java
@@ -0,0 +1,40 @@
+package com.whl.hotelService.domain.common.entity;
+
+import com.whl.hotelService.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name="reservation")
+public class Reservation {
+    @Id
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="hotel_name", foreignKey = @ForeignKey(name="fk_reservation_hotel_name", foreignKeyDefinition = "FOREIGN KEY(hotel_name) REFERENCES hotel(name) ON DELETE CASCADE ON UPDATE CASCADE"), nullable = false)
+    private Hotel hotelName; // 호텔 정보
+
+    @ManyToOne
+    @JoinColumn(name="room_id", foreignKey = @ForeignKey(name="fk_reservation_hotel_name", foreignKeyDefinition = "FOREIGN KEY(room_id) REFERENCES room(id) ON DELETE CASCADE ON UPDATE CASCADE"), nullable = false)
+    private Room roomId; // 객실 정보
+
+    @ManyToOne
+    @JoinColumn(name="user_id", foreignKey = @ForeignKey(name="fk_reservation_hotel_name", foreignKeyDefinition = "FOREIGN KEY(user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE"), nullable = false)
+    private User user_id; // 사용자 정보
+
+    @Column(nullable = false)
+    private Date checkin; // 체크인
+    @Column(nullable = false)
+    private Date checkout; // 체크아웃
+    @Column(nullable = false)
+    private String status; // 예약 상태
+}
