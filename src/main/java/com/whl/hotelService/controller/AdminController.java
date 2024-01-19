c6f7efaf78dbeb4bf26e49337c9eb274d16f437a
98Woonho <9woonho8@gmail.com>
2024-01-19 18:52:54 +0900

    .

--- src/main/java/com/whl/hotelService/controller/AdminController.java
+++ src/main/java/com/whl/hotelService/controller/AdminController.java
@@ -1,16 +1,24 @@
 package com.whl.hotelService.controller;
 
+import com.whl.hotelService.domain.common.entity.Reservation;
+import com.whl.hotelService.domain.common.repository.ReservationRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Slf4j
 @Controller
 @RequestMapping(value ="admin")
 public class AdminController {
+    @Autowired
+    private ReservationRepository reservationRepository;
+
     @GetMapping("reservationStatus")
     public void getReservationStatus(@RequestParam(value="region") String region,
                                      Model model) {
@@ -25,4 +33,10 @@ public class AdminController {
 
     @GetMapping("deleteHotel")
     public void getDeleteHotel() {}
+
+    @GetMapping("sales")
+    public void getSales(Model model) {
+        List<Reservation> list = reservationRepository.findReservationsInNovember();
+        model.addAttribute("list", list);
+    }
 }
---
+++ src/main/java/com/whl/hotelService/domain/common/repository/ReservationRepository.java
@@ -0,0 +1,14 @@
+package com.whl.hotelService.domain.common.repository;
+
+import com.whl.hotelService.domain.common.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    @Query("SELECT r FROM Reservation r WHERE MONTH(r.checkin) = 11")
+    List<Reservation> findReservationsInNovember();
+}
