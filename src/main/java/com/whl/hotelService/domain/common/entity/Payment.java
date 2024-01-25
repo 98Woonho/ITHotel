9ff8cc89378a470dea96f2fbcfa4feb55146e302
hbsh2330 <hbsh2330@naver.com>
2024-01-25 22:03:43 +0900

    게시판 수정

--- src/main/java/com/whl/hotelService/domain/common/entity/Payment.java
+++ src/main/java/com/whl/hotelService/domain/common/entity/Payment.java
@@ -22,11 +22,10 @@ public class Payment {
 
     @ManyToOne
     @JoinColumn(name="reservation_id", foreignKey = @ForeignKey(name="fk_payment_reservation_id", foreignKeyDefinition = "FOREIGN KEY(reservation_id) REFERENCES reservation(id) ON DELETE CASCADE ON UPDATE CASCADE"), nullable = false)
-
     private Reservation reservation_id; // 예약 정보
 
     @ManyToOne
-    @JoinColumn(name="user_user_id", foreignKey = @ForeignKey(name="fk_payment_user_user_id", foreignKeyDefinition = "FOREIGN KEY(user_user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE"), nullable = false)
+    @JoinColumn(name="user_id", foreignKey = @ForeignKey(name="fk_payment_user_id", foreignKeyDefinition = "FOREIGN KEY(user_id) REFERENCES user(userid) ON DELETE CASCADE ON UPDATE CASCADE"), nullable = false)
     private User userid; // 사용자 정보
 
     // 결제 정보
