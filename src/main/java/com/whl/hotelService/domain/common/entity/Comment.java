f006b07f720139f0b585ca231fbd6d3921b116f9
hbsh2330 <hbsh2330@naver.com>
2024-03-04 00:37:43 +0900

    entity 각주

--- src/main/java/com/whl/hotelService/domain/common/entity/Comment.java
+++ src/main/java/com/whl/hotelService/domain/common/entity/Comment.java
@@ -17,18 +17,18 @@ public class Comment extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id; // 인덱스 번호
 
     @Column(nullable = false)
-    private String content;
+    private String content; // 관리자 댓글
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "admin_board_id", foreignKey = @ForeignKey(name = "fk_comment_admin_board_id", foreignKeyDefinition = "FOREIGN KEY(admin_board_id) REFERENCES admin_board(id) ON DELETE CASCADE ON UPDATE CASCADE"), nullable = false)
-    private AdminBoard adminBoard;
+    private AdminBoard adminBoard; // 관리자게시판을 부모로 댓글을 외래키로 걸어 관리자게시판 삭제시 댓글도 함께 삭제하기 위한 join
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name="fk_comment_user_id", foreignKeyDefinition = "FOREIGN KEY(user_id) REFERENCES user(userid) ON DELETE CASCADE ON UPDATE CASCADE"), nullable = false)
-    private User user;
+    private User user; // 관리자가 댓글 작성했을 시, 본인이 쓴 댓글을 수정 및 삭제하기 위한 entity
 
     public void update(String content) {
         this.content = content;
--- src/main/java/com/whl/hotelService/domain/common/entity/NoticeBoardFileInfo.java
+++ src/main/java/com/whl/hotelService/domain/common/entity/NoticeBoardFileInfo.java
@@ -16,11 +16,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class NoticeBoardFileInfo extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id; // 파일 인덱스
     private String originalFileName; // 원본 파일 이름
     private String storedFileName; // 서버 저장 파일 이름
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="notice_board_id", foreignKey = @ForeignKey(name="fk_notice_board_file_info_notice_board_id", foreignKeyDefinition = "FOREIGN KEY(notice_board_id) REFERENCES notice_board(id) ON DELETE CASCADE ON UPDATE CASCADE"))
-    private NoticeBoard noticeBoard;
+    private NoticeBoard noticeBoard;  // 공지게시글을 부모키로 fileInfo를 외래키를 걸어 공지게시글이 삭제되었을 시 파일도 함께 삭제하기 위한 entity
 
 }
